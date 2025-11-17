34ccccd602bf748c4d2e6871d5cc246c316ebf5e
johnmart19 <johnmart19@users.noreply.github.com>
2025-11-17 15:08:34 +0000

    cmi: Patch audio library for VNDK 34
    VNDK 36 crashing:
    Revision: '0'
    ABI: 'arm'
    Timestamp: 2025-06-19 09:37:19.685329738+0300
    Process uptime: 0s
    Cmdline: /vendor/bin/hw/android.hardware.audio.service
    pid: 1093, tid: 1180, name: HwBinder:1093_2  >>> /vendor/bin/hw/android.hardware.audio.service <<<
    uid: 1041
    signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x00000040
    Cause: null pointer dereference
        r0  00000000  r1  f3cc0730  r2  00000003  r3  00000003
        r4  f418f780  r5  00000024  r6  00000001  r7  00000000
        r8  f3aacbf4  r9  f3f8600c  r10 f418f780  r11 f418f780
        ip  f2166a38  sp  f27ff4f8  lr  f216409f  pc  f2133588
    
    Requires Evolution-X/hardware_lineage_compat@51c1ec2 to work

--- extract-files.py
+++ extract-files.py
@@ -25,7 +25,7 @@ blob_fixups: blob_fixups_user_type = {
             b'liba2dpoffload_cmi.so\x00\x00\x00\x00\x00\x00\x00\x00',
         )
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
-    ('vendor/lib/libaudioroute_ext.so'): blob_fixup()
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
